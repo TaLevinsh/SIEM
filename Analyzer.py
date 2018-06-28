db93e1c6536647c40da51bfa40c3bd13b96d5156
TalSaban <Talsabana@gmail.com>
2018-06-29 00:21:30 +0300

    Added Files

--- Analyzer.py
+++ Analyzer.py
@@ -1,7 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-
 User = 'root'
 Password = 'P@ssw0rd'
 Host = '192.168.191.129'
@@ -32,9 +31,9 @@ def SpecificPort():
         if IP not in IP_Lst:
             IP_Lst.append(IP)
     if len(IP_Lst) > 0:
-        print 'These IP addresses:', IP_Lst, 'attampted to connect suspicious ports (444 / 4445)'
-    else:
-        print 'No port attack'
+        print 'These IP addresses:', IP_Lst, 'attempted to connect suspicious ports (444 / 4445)'
+    elif len(IP_Lst) == 0:
+        print 'No suspicious port attack'
     Cnx.commit()
     Cursor.close()
     Cnx.close()
@@ -60,8 +59,83 @@ def PortScan():
             Ports_Num2 = int(Ports_Num1[0])
             if Ports_Num2 >= 10:
                 print 'This IP Address', New_IP, 'Attempted To Connect To', Ports_Num1, 'Different Ports'
+    else:
+        print 'No Port-Scan Attack'
+    Cnx.commit()
+    Cursor.close()
+    Cnx.close()
+
+
+def PingSweep():
+    Cnx, Cursor = ConnectToDB()
+    Query1 = 'SELECT SRC_IP FROM fwlogs'
+    Cursor.execute(Query1)
+    IP_Lst = []
+    for IP in Cursor:
+        if IP not in IP_Lst:
+            IP_Lst.append(IP)
+    New_IP_Lst = []
+    for Item in IP_Lst:
+        New_IP = "".join(Item)
+        if New_IP not in New_IP_Lst:
+            New_IP_Lst.append(New_IP)
+    for New_IP in New_IP_Lst:
+        Query2 = 'SELECT COUNT(DISTINCT DST_IP) FROM fwlogs WHERE SRC_IP=' + "'" + New_IP + "'" + 'AND PORT=0'
+        Cursor.execute(Query2)
+        for Dst_IP1 in Cursor:
+            Dst_IP2 = int(Dst_IP1[0])
+            if Dst_IP2 >= 10:
+                print 'This IP Address', New_IP, 'Attempted To Ping', Dst_IP1, 'Different Hosts'
+    else:
+        print 'No Ping-Sweep Attack'
+    Cnx.commit()
+    Cursor.close()
+    Cnx.close()
+
+
+def GetTimeDifferences(Start, End):
+    C = End - Start
+    return divmod(C.days * 86400 + C.seconds, 60)
+
+
+def PingSweepTime():
+    Cnx, Cursor = ConnectToDB()
+    Query1 = 'SELECT SRC_IP FROM fwlogs'
+    Cursor.execute(Query1)
+    IP_Lst = []
+    for IP in Cursor:
+        if IP not in IP_Lst:
+            IP_Lst.append(IP)
+    New_IP_Lst = []
+    for Item in IP_Lst:
+        New_IP = "".join(Item)
+        if New_IP not in New_IP_Lst:
+            New_IP_Lst.append(New_IP)
+    for New_IP in New_IP_Lst:
+        Query2 = 'SELECT DISTINCT (DST_IP), DATE FROM fwlogs WHERE SRC_IP=' + "'" + New_IP + "'" + 'ORDER BY DATE DESC LIMIT 1'     # MAX
+        Cursor.execute(Query2)
+        TIME_LST = []
+        for MaxTIME in Cursor:
+            MaxTIME1 = MaxTIME[1]
+            if MaxTIME1 not in TIME_LST:
+                TIME_LST.append(MaxTIME1)
+        Query3 = 'SELECT DISTINCT (DST_IP), DATE FROM fwlogs WHERE SRC_IP=' + "'" + New_IP + "'" + 'ORDER BY DATE LIMIT 1'     # MIN
+        Cursor.execute(Query3)
+        for MinTIME in Cursor:
+            MinTIME1 = MinTIME[1]
+            if MinTIME1 not in TIME_LST:
+                TIME_LST.append(MinTIME1)
+
+            TimeDifference = GetTimeDifferences(TIME_LST[1], TIME_LST[0])
+            if TimeDifference <= (0, 10):
+                Query4 = 'SELECT COUNT(DISTINCT DST_IP) FROM fwlogs WHERE SRC_IP=' + "'" + New_IP + "'" + 'AND PORT=0'
+                Cursor.execute(Query4)
+                for Dst_IP1 in Cursor:
+                    Dst_IP2 = int(Dst_IP1[0])
+                    if Dst_IP2 >= 10:
+                        print 'This IP Address', New_IP, 'Attempted To Ping', Dst_IP1, 'Different Hosts In Less Than 10 Seconds'
             else:
-                break   # print 'No Port-Scan Attack'
+                print 'No Ping-Sweep Attack In Less Than 10 Seconds'
     Cnx.commit()
     Cursor.close()
     Cnx.close()
@@ -69,8 +143,12 @@ def PortScan():
 
 def main():
     Cnx, Cursor = ConnectToDB()
-    print SpecificPort()
-    print PortScan()
+
+    SpecificPort()
+    PortScan()
+    PingSweep()
+    PingSweepTime()
+
     Cursor.close()
     Cnx.close()
 
